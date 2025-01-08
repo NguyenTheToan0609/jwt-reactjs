4299e05beb6402a7af7e8d87c448676b6edf380e
NguyenTheToan0609 <thetoan0609@gmail.com>
2025-01-08 20:44:07 +0700

    Tao form login

--- src/component/login/login.js
+++ src/component/login/login.js
@@ -1,9 +1,37 @@
 import React from "react";
-
+import "./login.scss";
 const Login = (props) => {
   return (
     <div className="login-container">
-      <div>Login</div>
+      <div className="container">
+        <div className="row">
+          <div className="content-left col-7">
+            <div className="brand">FULLSTACK - JWT</div>
+            <div className="detail">
+              Connect with friends and the world around you
+            </div>
+          </div>
+
+          <div className="content-right col-5">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email or phone number"
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center ">Forgot password?</span>
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success">Create new account</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
